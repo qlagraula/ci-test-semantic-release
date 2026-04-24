259f8ddbb8b29d9cb12454d927ddfb5343adf2db
Quentin Lagraula <quentin.lagraula@gmail.com>
2026-04-24 17:32:57 +0200

    fix: bump patch

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ console.log('Hello world! This is a test of the CI pipeline.');
 console.log(
   'This file is used to trigger the CI workflow when a pull request is opened or updated.',
 );
+console.log(
+  "It's not meant to do anything functional, just to ensure that the CI pipeline is working correctly.",
+);
